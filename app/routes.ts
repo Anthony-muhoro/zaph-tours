78e5f36fc861255d49a5b5157c0602bc17e57cab
Anthony-muhoro <anthonymuhoro2003@gmail.com>
2025-07-05 17:12:27 +0300

    clean routes

--- app/routes.ts
+++ app/routes.ts
@@ -9,8 +9,6 @@ export default [
     route("/destinations/:id", "pages/client/destinationsdetails.tsx"),
     route("/trips/:id/blog", "pages/client/tripblog.tsx"),
   ]),
-  route("/sign-up", "pages/client/Signup.tsx"),
-  route("/sign-in", "pages/client/Signin.tsx"),
   layout("layouts/clientDashboardLayout.tsx", [
     route("/dashboard", "pages/clientdashboard/dashboard.tsx"),
   ]),
